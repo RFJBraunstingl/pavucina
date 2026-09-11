db9f1958eed51270d725a03fb2646ddcacbd8a1d
Roman Braunstingl <r.braunstingl@celix.at>
2026-09-11 14:15:16 +0200

    improve release script

--- release.sh
+++ release.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 LAST_TAG=$(git describe --tags --abbrev=0) || exit 1
 RELEASE_MESSAGES=$(git log --reverse --format=%s "${LAST_TAG}..HEAD") || exit 1
-VERSION="$1" perl -pi -e 's/^  "version": "[^"]*",/  "version": "$ENV{VERSION}",/' package.json
+RELEASE_MESSAGES=$(awk '!seen[$0]++' <<< "$RELEASE_MESSAGES")
+VERSION="${1:-$(node -p 'const [major, minor, patch] = require("./package.json").version.split("."); `${major}.${minor}.${Number(patch) + 1}`')}"
+VERSION="$VERSION" perl -pi -e 's/^  "version": "[^"]*",/  "version": "$ENV{VERSION}",/' package.json
 npm update
 git add package.json
 git add package-lock.json
-git commit -m "release v$1" -m "$RELEASE_MESSAGES"
+git commit -m "release v$VERSION" -m "$RELEASE_MESSAGES"
 git push
-git tag -s $1 -m "release v$1"
+git tag -s "$VERSION" -m "release v$VERSION"
 git push --tags
