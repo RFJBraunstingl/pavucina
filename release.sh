75978ef9862fe290efc85b989b33b23af9237e87
RFJBraunstingl <rfj.braunstingl@gmail.com>
2026-09-15 07:53:24 +0200

    do not update lock on every run

--- release.sh
+++ release.sh
@@ -4,8 +4,8 @@ RELEASE_MESSAGES=$(git log --reverse --format=%s "${LAST_TAG}..HEAD") || exit 1
 RELEASE_MESSAGES=$(awk '!/^Merge / && !seen[$0]++' <<< "$RELEASE_MESSAGES")
 VERSION="${1:-$(node -p 'const [major, minor, patch] = require("./package.json").version.split("."); `${major}.${minor}.${Number(patch) + 1}`')}"
 VERSION="$VERSION" perl -pi -e 's/^  "version": "[^"]*",/  "version": "$ENV{VERSION}",/' package.json
-npm install
-npm update
+#npm install
+#npm update
 git add package.json
 git add package-lock.json
 git commit -m "release v$VERSION" -m "$RELEASE_MESSAGES"
